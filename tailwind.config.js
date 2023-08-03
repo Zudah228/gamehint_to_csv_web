71021e3b407c8d72bd60e4c987a7a1f71b45f270
Zudah228 <rezel95.hin@gmail.com>
2023-08-03 22:30:02 +0900

    feat: tailwind の色を設定

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,9 +1,13 @@
+// eslint-disable-next-line @typescript-eslint/no-var-requires
+const colors = require("tailwindcss/colors");
+
 /** @type {import('tailwindcss').Config} */
 export default {
   content: ["./src/**/*.{html,js,svelte,ts}"],
   theme: {
     extend: {},
     colors: {
+      ...colors,
       primary: {
         DEFAULT: "#ff3e00",
       },
